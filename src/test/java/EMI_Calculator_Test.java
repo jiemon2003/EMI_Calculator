a3b4224f93142c4456f4f9fa44094078fa1e2996
Jihan Emon <emon.esoft@gmail.com>
2022-01-01 00:49:27 +0600

    1st

--- src/test/java/EMI_Calculator_Test.java
+++ src/test/java/EMI_Calculator_Test.java
@@ -5,7 +5,6 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
@@ -19,7 +18,7 @@ public class EMI_Calculator_Test {
 
         DesiredCapabilities dc = new DesiredCapabilities();
         dc.setCapability("platformName", "Android");
-        dc.setCapability("deviceName", "Galaxy_J4");
+        dc.setCapability("deviceName", "Redmi_Note7");
         dc.setCapability("udid", "42001cc6f0a97535");
         dc.setCapability("PlatformVersion", "9.0.0");
         dc.setCapability("appPackage", "com.sec.android.app.popupcalculator");
@@ -30,15 +29,59 @@ public class EMI_Calculator_Test {
     }
 
     @Test(priority = 0)
-    public static void Add_Operation() throws Exception {
+    public static void Enter_Loan_in_the_amount_field() throws Exception {
         MyScreenRecorder.stopRecording();
-        driver.findElement(By.id("com.sec.android.app.popupcalculator:id/bt_08")).click();
-        driver.findElement(By.xpath("//android.widget.Button[@content-desc=\"Plus\"]")).click();
-        driver.findElement(By.id("com.sec.android.app.popupcalculator:id/bt_01")).click();
-        driver.findElement(By.xpath("//android.widget.Button[@content-desc=\"Equal\"]")).click();
+
         MyScreenRecorder.stopRecording();
 
     }
+
+    @Test(priority = 1)
+    public static void Enter__Interest_Percent_rate_in_the_interest_field() throws Exception {
+        MyScreenRecorder.stopRecording();
+
+        MyScreenRecorder.stopRecording();
+
+    }
+
+    @Test(priority = 2)
+    public static void Enter_period_in_the_period_field_in_years() throws Exception {
+        MyScreenRecorder.stopRecording();
+
+        MyScreenRecorder.stopRecording();
+
+    }
+
+    @Test(priority = 3)
+    public static void Enter_pFee_percent_for_the_processing_fee() throws Exception {
+        MyScreenRecorder.stopRecording();
+
+        MyScreenRecorder.stopRecording();
+
+    }
+
+    @Test(priority = 4)
+    public static void And_tap_on_the_calculate_button() throws Exception {
+        MyScreenRecorder.stopRecording();
+
+        MyScreenRecorder.stopRecording();
+
+    }
+
+    @Test(priority = 5)
+    public static void Then_Verify_that_the_detail_results_are_correct() throws Exception {
+        MyScreenRecorder.stopRecording();
+
+        MyScreenRecorder.stopRecording();
+
+    }
+
+
+
+
+
+
+
 
     @AfterTest
     public static void close() {
